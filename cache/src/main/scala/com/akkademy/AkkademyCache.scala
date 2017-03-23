7b070d98068339c207c9d079907a9f1cd021b96d
drazens <drazen.skobic@mail.com>
2017-03-23 23:20:39 +0000

    Adding support to Cache for new message

--- cache/src/main/scala/com/akkademy/AkkademyCache.scala
+++ cache/src/main/scala/com/akkademy/AkkademyCache.scala
@@ -2,11 +2,13 @@ package com.akkademy
 
 import scala.collection.mutable.HashMap
 
+import com.akkademy.messages.Connected
 import com.akkademy.messages.GetRequest
 import com.akkademy.messages.KeyNotFoundException
 import com.akkademy.messages.SetRequest
 
 import akka.actor.Actor
+import akka.actor.ActorRef
 import akka.actor.Status
 import akka.event.Logging
 
@@ -16,21 +18,38 @@ class AkkademyCache extends Actor {
   val log = Logging(context.system, this)
 
   override def receive = {
-    case SetRequest(key, value, _) =>
-      log.info(s"received SetRequest - key: ${key} value: ${value}")
-      map.put(key, value)
-      sender() ! Status.Success
-    case GetRequest(key, _) =>
-      log.info(s"received GetRequest - key: ${key}")
-      map.get(key) match {
-        case None =>
-          sender() ! Status.Failure(KeyNotFoundException(key))
-        case Some(x) =>
-          sender() ! x
+    case x: Connected =>
+      sender() ! x
+    case SetRequest(key, value, senderRef) =>
+      handleSetRequest(key, value, senderRef)
+    case GetRequest(key, senderRef) =>
+      handleGetRequest(key, senderRef)
+    case x: List[_] =>
+      x foreach {
+        case SetRequest(key, value, senderRef) =>
+          handleSetRequest(key, value, senderRef)
+        case GetRequest(key, senderRef) =>
+          handleGetRequest(key, senderRef)
       }
     case o =>
       log.info(s"received unknown message: ${0}")
       sender() ! Status.Failure(new ClassNotFoundException)
+  }
+
+  def handleSetRequest(key: String, value: Object, senderRef: ActorRef): Unit = {
+    log.info(s"received SetRequest - key: ${key} value: ${value}")
+    map.put(key, value)
+    senderRef ! Status.Success
+  }
+
+  def handleGetRequest(key: String, senderRef: ActorRef): Unit = {
+    log.info(s"received GetRequest - key: ${key}")
+    map.get(key) match {
+      case None =>
+        senderRef ! Status.Failure(KeyNotFoundException(key))
+      case Some(x) =>
+        senderRef ! x
+    }
   }
 
 }
--- cache/src/test/scala/com/akkademy/AkkademyCacheSpec.scala
+++ cache/src/test/scala/com/akkademy/AkkademyCacheSpec.scala
@@ -4,22 +4,48 @@ import org.scalatest.FunSpecLike
 import org.scalatest.Matchers
 
 import com.akkademy.messages.SetRequest
+import com.typesafe.config.ConfigFactory
 
 import akka.actor.ActorSystem
+import akka.actor.Status
 import akka.testkit.TestActorRef
+import akka.testkit.TestProbe
 
 class AkkademyCacheSpec extends FunSpecLike with Matchers {
 
-  implicit val system = ActorSystem()
+  implicit val system = ActorSystem("akkademyCache-test", ConfigFactory.empty)
 
   describe("akkademyCache") {
     describe("givenSetRequest") {
+      val testProbe = TestProbe()
+
       it("should place key/value into map") {
         val actorRef = TestActorRef(new AkkademyCache)
-        actorRef ! SetRequest("key", "value")
+        actorRef ! SetRequest("key", "value", testProbe.ref)
+
         val akkademyCache = actorRef.underlyingActor
         akkademyCache.map.get("key") should equal(Some("value"))
+
+        testProbe expectMsg Status.Success
       }
+    }
+  }
+
+  describe("given List[SetRequest]") {
+    it("should place key/values int mpa") {
+      val testProbe = TestProbe()
+
+      val actorRef = TestActorRef(new AkkademyCache)
+      actorRef ! List(
+        SetRequest("key1", "value1", testProbe.ref),
+        SetRequest("key2", "value2", testProbe.ref))
+
+      val akkademyCache = actorRef.underlyingActor
+      akkademyCache.map.get("key1") should equal(Some("value1"))
+      akkademyCache.map.get("key2") should equal(Some("value2"))
+
+      testProbe expectMsg Status.Success
+      testProbe expectMsg Status.Success
     }
   }
 
