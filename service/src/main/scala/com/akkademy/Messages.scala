546b330880c3941b536f055d388b995568207df3
drazens <drazen.skobic@mail.com>
2017-04-10 21:01:34 +0100

    Refactoring service layer

--- service/src/main/scala/com/akkademy/Messages.scala
+++
@@ -1,6 +0,0 @@
-package com.akkademy
-
-case class ParseArticle(url: String)
-case class HttpResponse(htmlString: String)
-case class ParseHtmlArticle(url: String, htmlString: String)
-case class ArticleBody(url: String, body: String)
